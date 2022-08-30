969849a61c95b2f104159dd428ab0f3bbff06dca
numdo <kdhehsl@gmail.com>
2022-08-30 09:11:44 +0900

    user 데이터 자동생성 코드 완성

--- src/main/java/com/example/testsearch/controller/AdminController.java
+++ src/main/java/com/example/testsearch/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.example.testsearch.controller;
 import com.example.testsearch.dto.Data;
 import com.example.testsearch.dto.ImageFile;
 import com.example.testsearch.dto.UserDto;
+import com.example.testsearch.mapper.CommonMapper;
 import com.example.testsearch.mapper.UserMapper;
 import com.example.testsearch.service.AdminService;
 import com.example.testsearch.service.UserService;
@@ -10,10 +11,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
@@ -28,12 +26,13 @@ public class AdminController {
 
     private UserService userService;
     private AdminService adminService;
+    private CommonMapper commonMapper;
 
 
-    public AdminController(UserService userService, AdminService adminService){
+    public AdminController(UserService userService, AdminService adminService, CommonMapper commonMapper){
         this.userService = userService;
         this.adminService = adminService;
-
+        this.commonMapper = commonMapper;
     }
 
     /* 전체 회원정보 조회 */
@@ -53,11 +52,11 @@ public class AdminController {
     //data 등록화면
     @GetMapping("/insert")
     public String addImage(Model model, HttpSession session) {
-
+        String sessionId = (String) session.getAttribute("SID");
         String sessionEmail = (String) session.getAttribute("SEMAIL");
         model.addAttribute("title", "작품 등록");
         model.addAttribute("SEMAIL",sessionEmail);
-
+        model.addAttribute("SID",sessionId);
         return "admin/insert";
     }
 
@@ -69,6 +68,7 @@ public class AdminController {
             , HttpServletRequest request) {
 
         String sessionEmail = (String) session.getAttribute("SEMAIL");
+        String sessionId = (String) session.getAttribute("SID");
 
         //파일 업로드
         String serverName = request.getServerName();
@@ -82,12 +82,11 @@ public class AdminController {
 
         logger.info("data 등록 폼에서 입력받은 데이터: {}", data);
 
-        adminService.addData(data, sessionEmail, fileImage, fileRealPath);
+        adminService.addData(data, sessionId, fileImage, fileRealPath);
 
 
         return "redirect:/test/list2";
 
     }
-
 
 }
